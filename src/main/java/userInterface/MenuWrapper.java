2aa27847b179da7e07ddb80cd3f9485ae67991c1
dingdalton <daltonhtx@gmail.com>
2020-10-07 17:00:54 -0700

    added restaurant UI

---
+++ src/main/java/userInterface/MenuWrapper.java
@@ -0,0 +1,15 @@
+package userInterface;
+
+import java.util.ArrayList;
+
+public class MenuWrapper {
+
+	   private ArrayList<MenuItem> itemList;
+
+	   public ArrayList<MenuItem> getItemList() {
+	      return itemList;
+	   }
+	   public void setItemList(ArrayList<MenuItem> menuItems) {
+	      this.itemList = menuItems;
+	   }
+	}
